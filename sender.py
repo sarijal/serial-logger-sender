13ac737851d56ee150b983fc3bfe363d42fbc9b3
sarijalou <ata.sarijalou@gmail.com>
2024-09-07 19:29:53 +0330

    send data from hex ok

--- sender.py
+++ sender.py
@@ -3,8 +3,8 @@ import csv
 import time
 from datetime import datetime
 
-serial_port = 'COM6'   # For WIN
-# serial_port='/dev/ttyUSB1'  # For Linux
+# serial_port = 'COM6'   # For WIN
+serial_port='/dev/ttyUSB1'  # For Linux
 
 baud_rate = 115200  
 csv_file = "serial_log.csv"
@@ -12,7 +12,9 @@ csv_file = "serial_log.csv"
 ser = serial.Serial(serial_port, baud_rate)
 
 def send_data(serial_conn, data):
-    serial_conn.write(data.encode())
+    # serial_conn.write(data.encode())
+    serial_conn.write(data)
+
 
 with open(csv_file, 'r') as file:
     reader = csv.reader(file)
@@ -21,8 +23,11 @@ with open(csv_file, 'r') as file:
     
     for row in reader:
         if row:
-            timestamp_str, hex_data, plain_data = row
+            timestamp_str, hex_str, plain_data = row
             
+
+            hex_data = bytes.fromhex(hex_str)
+
             timestamp = datetime.strptime(timestamp_str, '%Y-%m-%d %H:%M:%S.%f')
             
             if previous_timestamp:
@@ -31,8 +36,8 @@ with open(csv_file, 'r') as file:
                 if time_gap > 0:
                     time.sleep(time_gap)
             
-            print(f"Sending at {datetime.now()}: {plain_data}")
-            send_data(ser, plain_data)
+            print(f"Sending at {datetime.now()}: {hex_data}")
+            send_data(ser, hex_data)
             
             previous_timestamp = timestamp
 
